5f05f8754469b4a0c4bc40fd090646588dac5afa
NicoUmansky <umanskynico@gmail.com>
2022-04-21 14:16:18 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int counter = 1;
+        int countermax = 100;
+        while (counter <= countermax)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    public int n1, n2;
+
+    void Start()
+    {
+
+        if (n1 != n2)
+        {
+            while (n1 <= n2)
+            {
+                Debug.Log(n1);
+                n1++;
+            }
+        }
+        else
+        {
+            Debug.Log("Son el mismo numero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
